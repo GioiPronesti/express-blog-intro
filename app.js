35e9a1537ec398f6e09bd6a46f5827b287c2fdd1
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-18 14:58:32 +0100

    first commit add structure node starts file

---
+++ app.js
@@ -0,0 +1,8 @@
+/*Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità 
+1) - Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto “Server del mio blog”
+2) - Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un   array di stringhe)
+3) - Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array.
+4) - Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+Testare su postman*/
+
+//console.log("We are in app.js");
