63a09e51564e11a48bce4e54b711ef866ac21fad
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-18 18:35:01 +0100

    fix: array of postList and add bacheca root to show response objcts

--- app.js
+++ app.js
@@ -17,39 +17,44 @@ const port = 3000; // questa è la porta per comunicare con il server!
 const postList = [
   {
     title: "Ciambellone",
-    content: content,
-    img: img,
-    strtags: (tags = ["ricetta"]),
+    content: "content",
+    img: "./assets/img-blog/ciambellone.jpeg",
+    tags: (arrayTags = ["ricetta", "preparazione", "consigli"]),
   },
 
   {
     title: "Cracker Barbabietola",
-    content: content,
-    img: img,
-    strtags: (tags = ["ricetta"]),
+    content: "content",
+    img: "./assets/img-blog/cracker_barbabietola.jpeg",
+    tags: (arrayTags = ["ricetta", "preparazione", "consigli"]),
   },
 
   {
     title: "Pane fritto dolce",
-    content: content,
-    img: img,
-    strtags: (tags = ["ricetta"]),
+    content: "content",
+    img: "./assets/img-blog/pane_fritto_dolce.jpeg",
+    tags: (arrayTags = ["ricetta", "preparazione", "consigli"]),
   },
 
   {
     title: "Pasta barbabietola",
-    content: content,
-    img: img,
-    strtags: (tags = ["ricetta"]),
+    content: "content",
+    img: "./assets/img-blog/pasta_barbabietola.jpeg",
+    tags: (arrayTags = ["ricetta", "preparazione", "consigli"]),
   },
 
   {
     title: "Torta paesana",
-    content: content,
-    img: img,
-    strtags: (tags = ["ricetta"]),
+    content: "content",
+    img: "./assets/img-blog/torta_paesana.jpeg",
+    tags: (arrayTags = ["ricetta", "preparazione", "consigli"]),
   },
 ];
+
+let postListLenght = postList.length;
+
+// gestisco asset statico sulla cartella pubblic resa accessibile dall' express.static
+app.use(express.static("pubblic"));
 
 // 1)
 // definisco la prima rotta di default per accedere al server,
@@ -57,6 +62,15 @@ const postList = [
 // con il metodo GET
 app.get("/", (req, res) => {
   res.send("Server del mio blog");
+});
+
+app.get("/bacheca", (req, res) => {
+  console.log("Questa è la bacheca");
+
+  const response = {
+    results: [],
+    count: postListLenght,
+  };
 });
 
 // qui avvio il mio server e gli dico di rimanere in ascolto sulla porta 3000 del local host
